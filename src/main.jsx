09c7e166f97c484bd31759b4f9cab3b59e083e2e
clement <xenobyte012@gmail.com>
2026-03-28 22:17:11 +0200

    Add GitHub Pages deployment config

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/dave-academy">
+    <BrowserRouter basename="/dave-academy-site">
       <App />
     </BrowserRouter>
   </React.StrictMode>
